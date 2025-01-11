67fcff201a64d2ea757e023a40b4ef457f5e9009
ChaitanyaSn <chaitanyascode24@gmail.com>
2025-01-11 14:17:29 +0530

    err

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../context/AuthContext.jsx';
+import { useAuth } from '../../context/AuthContext';
 import toast from 'react-hot-toast';
 
 function Login() {
