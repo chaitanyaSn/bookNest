9a95b70bdded099db9cee14fa586669a55032da1
ChaitanyaSn <chaitanyascode24@gmail.com>
2025-01-11 14:59:42 +0530

    Ensure AuthContext file is in the correct directory

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.jsx', '.json'],
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
-})
+      '@': '/src',  // Configure the '@' alias to point to the src folder
+    },
+  },
+});
