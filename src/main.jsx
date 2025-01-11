40de9a2f26dfb68909457eddd2700a8c0cb27b21
ChaitanyaSn <chaitanyascode24@gmail.com>
2025-01-11 14:31:16 +0530

    main auth

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,15 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import { AuthProvider } from "./context/AuthContext";
+
 
 createRoot(document.getElementById("root")).render(
-  <AuthProvider>
+ 
     <StrictMode>
       <BrowserRouter>
         <App />
         <Toaster position="top-right" reverseOrder={false} />
       </BrowserRouter>
     </StrictMode>
-  </AuthProvider>
+
 );
